16ad3666f1b2a5d37df6ba3c75f756905dc9a462
Meetkumar Patel <meetp1197@gmail.com>
2017-09-15 02:06:40 +0530

    Change in controller

--- SMC/application/controllers/adwaita.php
+++ SMC/application/controllers/adwaita.php
@@ -3,12 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Adwaita extends CI_Controller {
 
-	public function index()
-	{
-		$this->load->view('header');
-		$this->load->view('app/timeline/time');
-		$this->load->view('footer');		
-	}
+
 	
 
 }
